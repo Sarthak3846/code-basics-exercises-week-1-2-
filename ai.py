4a025f1626eae0fdc6a4e09ee56fff44dddc3af7
Sarthak3846 <sarthaktyagi3846@gmail.com>
2024-05-05 00:22:10 +0530

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,6 @@
+def calculate_area():
+   base = int(input("enter the base of the triangle:"))
+   height = int(input("enter height of the triangle:"))
+   area = 0.5*base*height
+   print("area of the triangle is:",area)
+calculate_area()
